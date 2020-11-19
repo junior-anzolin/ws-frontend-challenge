197353d0b04cf143beddf916852912af60e9896f
Junior Anzolin <junioranzolin58@gmail.com>
2020-11-18 21:50:35 -0300

    Inicio do projeto

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/pages/home.css';
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home;
